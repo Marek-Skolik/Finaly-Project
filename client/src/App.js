19f2cc08311befa72f5c4ec61c3ddb67a0fcbf87
Marek Skolik <marek250296@interia.pl>
2023-04-17 20:27:14 +0200

    added frontend and backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/Views/Header/Header';
 import Footer from './components/Views/Footer/Footer';
+import HomePage from './components/Pages/HomePage/HomePage';
 
 function App() {
   return (
-    <Container>
-      <Header />
-      <Footer />
-    </Container>
+    <div>
+      <Router>
+        <Container>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage/>}/> 
+          </Routes>  
+          <Footer /> 
+        </Container>
+      </Router>  
+    </div>
   );
 }
 
